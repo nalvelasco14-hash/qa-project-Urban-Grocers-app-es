0c5f1fc0b10ab778e5355a8a63dcdb894bcbf20f
Nataly Alvarez Velasco <nalvelasco14@gmail.com>
2025-10-03 12:32:12 -0700

    Url_service actualizada

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-1c4606ca-07be-4476-80fd-ec05f25c25ae.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-8f807fba-0ee4-456a-b221-080ee6cc0f28.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users/"
 KITS_PATH = "/api/v1/kits/"
